bb859eff0c7d5446a01f9135dece5af598d3c540
Giulia <sindonigiulia@gmail.com>
2021-06-15 17:45:53 +0100

    no relevant changes

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,7 @@
+import numpy
+import time
+import matplotlib.pyplot as plt
+
 def quick_sort(array):
 	if len(array) <= 1:
 		return array
@@ -37,10 +41,6 @@ ex1 = [4, 9, 3, -2, 9, 10, -5, 8, 4]
 # 		quick_sort_2(smaller)
 # 		quick_sort_2(greater)
 # 	return smaller + equal + greater
-
-
-
-print(quick_sort(ex1))
 
 
 
