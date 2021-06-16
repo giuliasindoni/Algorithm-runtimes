4b334278a52bbaab544d5f1249e0bc9d6fcab19b
Giulia <sindonigiulia@gmail.com>
2021-06-16 14:27:48 +0100

    commeted out everything except for algorithm def

--- selection_sort.py
+++ selection_sort.py
@@ -11,18 +11,18 @@ def selection_sort(array):
 
 
 
-x = [5, 9, 2, 50, -34]
+# x = [5, 9, 2, 50, -34]
 
 
-example = [-14, 7, 8, 6, -2, -1, -100]
+# example = [-14, 7, 8, 6, -2, -1, -100]
 
-example2 = [67, 0, -1, 90, 89, 5, 9, 7, 7]
-
-
-example3 = [0, 0, 90, 8, -2, 56, 0]
-
-example4 = [8, -3, 3]
+# example2 = [67, 0, -1, 90, 89, 5, 9, 7, 7]
 
 
-print(selection_sort(example))
+# example3 = [0, 0, 90, 8, -2, 56, 0]
+
+# example4 = [8, -3, 3]
+
+
+# print(selection_sort(example))
 
