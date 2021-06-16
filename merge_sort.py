06956fb4eb7a5879666c274d15755ddc86d0ddfc
Giulia <sindonigiulia@gmail.com>
2021-06-16 14:27:40 +0100

    commeted out everything except for algorithm def

--- merge_sort.py
+++ merge_sort.py
@@ -37,55 +37,55 @@ def merge_sort_2(myarray):
 		return(myarray)
 
 
-def create_n_random_arrays(n, mysize):
-	list_of_arrays = []
-	for i in range(n):
-		created_array = numpy.random.randint(-200, 200, size=mysize, dtype=int)
-		list_of_arrays.append(created_array)
-	return list_of_arrays
+# def create_n_random_arrays(n, mysize):
+# 	list_of_arrays = []
+# 	for i in range(n):
+# 		created_array = numpy.random.randint(-200, 200, size=mysize, dtype=int)
+# 		list_of_arrays.append(created_array)
+# 	return list_of_arrays
 
-list_of_avarages = []
+# list_of_avarages = []
 
-for i in [5,10, 50, 100, 500, 1000]:
-	lenght_i = create_n_random_arrays(10, i)
-	attempt = []
-	for array in lenght_i:
-		start_time = time.time()
-		merge_sort_2(array)
-		end_time = time.time()
-		time_taken = end_time - start_time
-		attempt.append(time_taken)
-	attempt_avarage_i = sum(attempt) / len(attempt)
-	list_of_avarages.append(attempt_avarage_i)
+# for i in [5,10, 50, 100, 500, 1000]:
+# 	lenght_i = create_n_random_arrays(10, i)
+# 	attempt = []
+# 	for array in lenght_i:
+# 		start_time = time.time()
+# 		merge_sort_2(array)
+# 		end_time = time.time()
+# 		time_taken = end_time - start_time
+# 		attempt.append(time_taken)
+# 	attempt_avarage_i = sum(attempt) / len(attempt)
+# 	list_of_avarages.append(attempt_avarage_i)
 
-#print(list_of_avarages)
+# #print(list_of_avarages)
 
-#simple check
+# #simple check
 
 	
-#if all(list_of_avarages[i] < list_of_avarages[i+1] for i in range(len(list_of_avarages)-1)):
-#	print("TRUE")
+# #if all(list_of_avarages[i] < list_of_avarages[i+1] for i in range(len(list_of_avarages)-1)):
+# #	print("TRUE")
 
 
-size = [5,10, 50, 100, 500, 1000]
+# size = [5,10, 50, 100, 500, 1000]
 
-avarage = list_of_avarages
+# avarage = list_of_avarages
 
-plt.plot(size, avarage)
-plt.xlabel('Size')
-plt.ylabel('Avarage')
-plt.show()
-
-
-
-#size = [5,10, 50, 100, 500, 1000, 5000]
+# plt.plot(size, avarage)
+# plt.xlabel('Size')
+# plt.ylabel('Avarage')
+# plt.show()
 
 
-#plt.plot(size, list_of_avarages_bubble , 'b-', label='bubble sort')
-#plt.plot(size, list_of_avarages_merge, 'r-', label='merge sort')
 
-#plt.xlabel('Size')
-#plt.ylabel('Avarage')
-#plt.show()
+# #size = [5,10, 50, 100, 500, 1000, 5000]
+
+
+# #plt.plot(size, list_of_avarages_bubble , 'b-', label='bubble sort')
+# #plt.plot(size, list_of_avarages_merge, 'r-', label='merge sort')
+
+# #plt.xlabel('Size')
+# #plt.ylabel('Avarage')
+# #plt.show()
 
 
