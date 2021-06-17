cebeff6b626000b01a78c34c611db59a70728c01
Giulia <sindonigiulia@gmail.com>
2021-06-17 14:00:51 +0100

    block of code commented out

--- sequential_search.py
+++ sequential_search.py
@@ -13,9 +13,9 @@ def sequential_search(array, key):
 	else:
 		return -1
 
-A = [9, 0, 2, 8, 10, 10]
+#A = [9, 0, 2, 8, 10, 10]
 
-K = 10
+#K = 10
 
 
 
@@ -23,73 +23,76 @@ K = 10
 
 
 
-def create_n_random_arrays(n, mysize):
-  	list_of_arrays = []
-  	for i in range(n):
-  		created_array = numpy.random.randint(-500000, 500000, size=mysize, dtype=int)
-  		list_of_arrays.append(created_array)
-  	return list_of_arrays
+# def create_n_random_arrays(n, mysize):
+#   	list_of_arrays = []
+#   	for i in range(n):
+#   		created_array = numpy.random.randint(-500000, 500000, size=mysize, dtype=int)
+#   		list_of_arrays.append(created_array)
+#   	return list_of_arrays
 
 
 
-def collect_search_data(algorithm, array_of_sizes):
-	list_of_averages_algorithm = []
-	for size in array_of_sizes:
-		lenght_size = create_n_random_arrays(10, size)
-		attempt = []
-		for array in lenght_size:
-			randomkey = random.choice(array)
-			start_time = time.time()
-			algorithm(array, randomkey)
-			end_time = time.time()
-			time_taken = end_time - start_time
-			attempt.append(time_taken)
-		attempt_average_size = sum(attempt) / len(attempt)
-		list_of_averages_algorithm.append(attempt_average_size)
-	return list_of_averages_algorithm
+# def collect_search_data(algorithm, array_of_sizes):
+# 	list_of_averages_algorithm = []
+# 	for size in array_of_sizes:
+# 		lenght_size = create_n_random_arrays(10, size)
+# 		attempt = []
+# 		for array in lenght_size:
+# 			randomkey = random.choice(array)
+# 			start_time = time.time()
+# 			algorithm(array, randomkey)
+# 			end_time = time.time()
+# 			time_taken = end_time - start_time
+# 			attempt.append(time_taken)
+# 		attempt_average_size = sum(attempt) / len(attempt)
+# 		list_of_averages_algorithm.append(attempt_average_size)
+# 	return list_of_averages_algorithm
 
-my_sizes = [5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 500000]
+# my_sizes = [5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 500000]
 
-list_of_averages_sequential = collect_search_data(sequential_search, my_sizes)
+# list_of_averages_sequential = collect_search_data(sequential_search, my_sizes)
 
-plt.xscale('log')
-plt.yscale('log')
+# plt.xscale('log')
+# plt.yscale('log')
 
-plt.plot(my_sizes, list_of_averages_sequential, 'ro-', label = 'sequential search')
-
-
-#--------Plotting some functions to appreciate the bounds of the algorithms curves
-
-x = range(5,500000)
-
-y = [(1/10**6)*math.log(i,2) for i in x]
-
-m = [(1/10**7)*i*math.log(i,2) for i in x]
-
-k =  [ (1/10**7)* i for i in x]
-
-#plt.plot(x, y, 'g--', label = 'O(logn)')
-
-plt.plot(x, k, 'b--', label = 'O(n)')
-
-plt.plot(x, m, 'g--', label = 'O(n*logn)')
+# plt.plot(my_sizes, list_of_averages_sequential, 'ro-', label = 'sequential search')
 
 
+# #--------Plotting some functions to appreciate the bounds of the algorithms curves
 
-#------Create legend
+# x = range(5,500000)
 
-plt.legend(loc='upper left')
-plt.xlabel('Size of array')
-plt.ylabel('Average (seconds)')
-plt.legend()
+# y = [(1/10**6)*math.log(i,2) for i in x]
+
+# m = [(1/10**7)*i*math.log(i,2) for i in x]
+
+# k =  [ (1/10**6)* i for i in x]
+
+# #plt.plot(x, y, 'g--', label = 'O(logn)')
+
+# plt.plot(x, k, 'b--', label = 'O(n)')
+
+# plt.plot(x, m, 'g--', label = 'O(n*logn)')
 
 
 
-#------- Saving image as .png 
+# #------Create legend
 
-#plt.savefig('merge-quick')
+# plt.legend(loc='upper left')
+# plt.xlabel('Size of array')
+# plt.ylabel('Average (seconds)')
+# plt.legend()
 
-plt.savefig('sequential-n*logn-n-comparison')
+
+
+# #------- Saving image as .png 
+
+# #plt.savefig('merge-quick')
+
+# plt.savefig('sequential-n*logn-n-comparison')
+
+
+
 
 
 
